9eedc8aaf3d6c028b76db928d0e40d8c217657b9
mykle jones <mykle.j91@gmail.com>
2019-11-25 17:01:31 -0500

    11/25/2019

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,19 +1,14 @@
 class LoginController < ApplicationController
-    def create
-        user_id = params[:message_sender_id]
-        messageCreated = Message.create(message_params)
-    
-    
-    
-        if user_id_from_token == user_id.to_i
-            render json: {ok:true, message: messageCreated}
+    def create 
+        user = User.find_by(username: params[:username].downcase)
+  
+
+        if user && user.authenticate(params[:password])
+           
+            render json: {token: create_token(user.id), user_id: user.id}
+            
         else
-            render json: {errors: messageCreated.errors.full_messages}, status: :unprocessable_entity
+            render json: {errors: ["password or username does not match"]}, status: :unprocessable_entity
         end
-    
     end
-        private
-        def message_params
-            params.permit(:message, :read, :message_reciever_id, :message_sender_id)
-        end
 end
