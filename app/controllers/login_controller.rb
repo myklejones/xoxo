d9bd4e37b4ce2ccd600f7e29848ede6f3bf15e3c
mykle jones <mykle.j91@gmail.com>
2019-11-26 18:46:51 -0500

    11/26/2019

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,5 +1,6 @@
 class LoginController < ApplicationController
     def create 
+       
         user = User.find_by(username: params[:username].downcase)
   
 
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :username, :email, :name, :photo, :age, :dob, :city_state, :about_me , :sex, :active, :conversation, :message, :picture, :created_at
+  attributes :id, :username, :email, :name, :photo, :age, :dob, :city_state, :about_me , :sex, :active, :conversations, :messages, :pictures, :created_at
 end
